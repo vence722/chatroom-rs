88e2d8287830aa8ce6fea71473004a57544c11f1
Vence Lin <vence.s.lin@beshk.net>
2025-12-05 14:44:08 +0800

    change Mutex<HashMap> to DashMap

--- chatroom-server/src/task/connection.rs
+++ chatroom-server/src/task/connection.rs
@@ -1,13 +1,10 @@
 use crate::types::ServerState;
 use std::sync::{Arc, atomic};
 use std::time::Duration;
-use tokio::sync::Mutex;
 
-pub(crate) async fn report_connection_count(state: Arc<Mutex<ServerState>>) -> anyhow::Result<()> {
+pub(crate) async fn report_connection_count(state: Arc<ServerState>) -> anyhow::Result<()> {
     loop {
         let connection_count = state
-            .lock()
-            .await
             .connections_count
             .load(atomic::Ordering::Relaxed);
         println!("Current connection count: {}", connection_count);
--- chatroom-server/src/task/message.rs
+++ chatroom-server/src/task/message.rs
@@ -1,21 +1,20 @@
 use crate::types::ServerState;
 use std::sync::Arc;
 use tokio::io::AsyncWriteExt;
-use tokio::sync::Mutex;
 
 pub(crate) async fn broadcast_message(
-    state: Arc<Mutex<ServerState>>,
+    state: Arc<ServerState>,
     conn_id: Arc<str>,
     msg: Vec<u8>,
 ) -> anyhow::Result<()> {
     // let mut broadcast_count = 0;
     let mut connection_refs = Vec::with_capacity(128);
     {
-        for (target_conn_id, target_stream) in state.lock().await.connections.iter() {
-            if *target_conn_id == conn_id {
+        for connection_entry in state.connections.iter() {
+            if *connection_entry.key() == conn_id {
                 continue;
             }
-            connection_refs.push(Arc::clone(target_stream));
+            connection_refs.push(Arc::clone(connection_entry.value()));
         }
     }
     let broadcast_count = connection_refs.len();
